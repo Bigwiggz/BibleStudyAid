997518e6d88241bcf50858b44a73b16a4841bcc0
Brian Wiggins <irdetection@hotmail.com>
2022-01-04 23:11:08 -0500

    added SqlScripts

---
+++ DBBibleStudyAid/TestScripts/sqlQueryScripts.sql
@@ -0,0 +1,10 @@
+﻿
+--Query Scripts
+
+
+SELECT * 
+FROM tblDailyBibleReading
+LEFT JOIN tblReferences ON PKIdtblDailyBibleReadings=[tblReferences].FKTableIdandName
+LEFT JOIN tblScriptures ON PKIdtblDailyBibleReadings=[tblScriptures].FKTableIdandName
+LEFT JOIN tblTagsToOtherTables ON PKIdtblDailyBibleReadings=[tblTagsToOtherTables].FKTableIdandName;
+
