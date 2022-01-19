1b5c9faf8faf204e18c876cf56e294bda421cc39
Brian Wiggins <irdetection@hotmail.com>
2022-01-19 06:57:23 -0500

    Adding Modal
    Adding Modal

---
+++ BibleStudyAidMVC/wwwroot/js/Views/DailyBibleReading/EditDailyBibleReading.js
@@ -0,0 +1,36 @@
+﻿/*
+ * Edit Daily Bible Reading
+ */
+
+//Events
+window.addEventListener("load", function () {
+    let referenceModal = new bootstrap.Modal(document.getElementById('referenceModal'), {
+        backdrop: true,
+        keyboard: false,
+        focus:true
+    });
+
+
+});
+
+
+//function to add eventlistener to reference modal
+function AddReferenceEditModal() {
+    const refTable = document.getElementsByClassName("referenceTableEdits");
+    for (let i = 0; i <= refTable.length; i++) {
+        refTable[i].addEventListener("click", () => {
+            //Get current row Id
+            const currentRowId = refTable.getAttribute("data-id");
+            document.getElementById("referenceId").setAttribute("value", currentRowId);
+            //Get current row Reference
+            const currentRowReference = refTable.closest("tr")[0].innerHTML;
+            document.getElementById("referenceCitation").setAttribute("value", currentRowReference);
+            //Get current row Reference Description
+            const currentRowReferenceDescription = refTable.closest("tr")[1].innerHTML;
+            document.getElementById("referenceDescription").setAttribute("value", currentRowReferenceDescription);
+            //show reference Modal
+            referenceModal.show();
+        });
+    };
+}
+
