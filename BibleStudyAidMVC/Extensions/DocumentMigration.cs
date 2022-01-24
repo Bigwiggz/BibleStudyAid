f4b41bc59b6935be4a186175c860bee1ca76dc1f
Brian Wiggins <irdetection@hotmail.com>
2022-01-24 07:09:52 -0500

    Adding Document Download Upload
    Adding Document Download Upload

--- BibleStudyAidMVC/Extensions/DocumentMigration.cs
+++ BibleStudyAidMVC/Extensions/DocumentMigration.cs
@@ -1,6 +1,8 @@
 ﻿
 using BibleStudyAidMVC.ViewModels;
 using BibleStudyDataAccessLibrary.Models;
+using ICSharpCode.SharpZipLib.Zip;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BibleStudyAidMVC.Extensions
 {
@@ -19,35 +21,106 @@ namespace BibleStudyAidMVC.Extensions
         {   //TODO: Setup a production and develop environment for blob storage
             //TODO: Figure out how to sanitize documents before uploading them
             string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, _configuration.GetSection("FileBlobStorage")["FolderName"]);
-            foreach (var viewModelDocument in viewModel)
+
+            if(model.Count > 0)
             {
-                var viewModelDocumentGUID = new Guid();
-                var uniqueFileName= $"{ viewModelDocumentGUID.ToString()}_{viewModelDocument.Document.FileName}";
-
-                foreach (var document in model)
+                foreach (var viewModelDocument in viewModel)
                 {
-                    document.ContentType = viewModelDocument.Document.ContentType;
-                    document.UniqueGUIDId = viewModelDocumentGUID;
-                    document.UniqueFileName = $"{ viewModelDocumentGUID.ToString()}_{viewModelDocument.Document.FileName}";
-                    document.FileName = viewModelDocument.Document.FileName;
-                    document.Name=viewModelDocument.Document.Name;
-                    document.ContentSize = viewModelDocument.Document.Length;
-                    document.ContentDisposition = viewModelDocument.Document.ContentDisposition;
-                }
-                string filePath=Path.Combine(uploadsFolder, uniqueFileName);
+                    var viewModelDocumentGUID = new Guid();
+                    var uniqueFileName = $"{ viewModelDocumentGUID.ToString()}_{viewModelDocument.Document.FileName}";
+                    for (int i = 0; i < model.Count; i++)
+                    {
+                        model[i].ContentType = viewModelDocument.Document.ContentType;
+                        model[i].UniqueGUIDId = viewModelDocumentGUID;
+                        model[i].UniqueFileName = $"{ viewModelDocumentGUID.ToString()}_{viewModelDocument.Document.FileName}";
+                        model[i].FileName = viewModelDocument.Document.FileName;
+                        model[i].Name = viewModelDocument.Document.Name;
+                        model[i].ContentSize = viewModelDocument.Document.Length;
+                        model[i].ContentDisposition = viewModelDocument.Document.ContentDisposition;
+                    }
 
-                using (var stream = new FileStream(filePath, FileMode.Create))
-                {
-                    await viewModelDocument.Document.CopyToAsync(stream);
-                    await stream.FlushAsync();
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await viewModelDocument.Document.CopyToAsync(stream);
+                        await stream.FlushAsync();
+                    }
+
                 }
-                
+            }
+            else
+            {
+                //TODO: Finish this error
+                throw;
             }
         }
 
-        public async Task DownloadMultipleFilesAsync(List<DocumentsViewModel> viewModel, List<Documents> model)
+        public async Task<byte[]> DownloadMultipleFilesAsync(List<DocumentsViewModel> viewModel, List<Documents> model)
         {
             string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, _configuration.GetSection("FileBlobStorage")["FolderName"]);
+            if(model.Count == 1)
+            {
+                string filePath=Path.Combine(uploadsFolder, model[0].UniqueFileName); 
+                byte[] fileBytes=await File.ReadAllBytesAsync(filePath);
+                
+            }
+            else
+            {
+                foreach (var document in model)
+                {
+
+                }
+                byte[] fileBytes = ;
+            }
+            return fileBytes;
+            //https://www.youtube.com/watch?v=DuAXUbxGcVc
+            //https://ourcodeworld.com/articles/read/629/how-to-create-and-extract-zip-files-compress-and-decompress-zip-with-sharpziplib-with-csharp-in-winforms
+
+            /*
+             * //Build the File Path.
+                string path = Path.Combine(this.Environment.WebRootPath, "Files/") + fileName;
+ 
+                //Read the File data into Byte Array.
+                byte[] bytes = System.IO.File.ReadAllBytes(path);
+ 
+                //Send the File to Download.
+                return File(bytes, "application/octet-stream", fileName);
+             */
+        }
+
+        private byte[] ZipMultipleFiles(List<Documents> model,string folderpath , int zipCompressionLevel)
+        {
+            var outputMemStream=new MemoryStream();
+            using(ZipOutputStream zipOutputStream=new ZipOutputStream(outputMemStream))
+            {
+                zipOutputStream.SetLevel(zipCompressionLevel);
+
+                byte[] buffer = new byte[4096];
+
+                foreach (var document in model)
+                {
+                    string filePath = Path.Combine(folderpath, document.FileName);
+
+                    var entry=new ZipEntry(filePath);
+
+                    entry.DateTime = DateTime.UtcNow;
+                    zipOutputStream.PutNextEntry(entry);
+
+                    using(FileStream fileStream = File.OpenRead(filePath))
+                    {
+                        int sourceBytes;
+                        do
+                        {
+                            sourceBytes = fileStream.Read(buffer, 0, buffer.Length);
+                            zipOutputStream.Write(buffer, 0, sourceBytes);
+                        } while (sourceBytes > 0);
+                    }
+                    zipOutputStream.Finish();
+                    zipOutputStream.Close();
+
+                }
+            }
         }
     }
 }
