4a7eb850e5fadaf740b7bcf9abda3e86f69b3c12
Brian Wiggins <irdetection@hotmail.com>
2022-02-03 06:25:27 -0500

    Added API Controller to load Tags information
    Added API Controller to load Tags information

---
+++ BibleStudyAidMVC/Controllers/TagsDataController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BibleStudyDataAccessLibrary.DataAccess;
+using BibleStudyDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibleStudyAidMVC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TagsDataController : ControllerBase
+    {
+        private readonly ITagsData _tagsData;
+        private readonly ILogger<Tags> _logger;
+        private readonly IMapper _mapper;
+
+        public TagsDataController(ITagsData tagsData, ILogger<Tags> logger, IMapper mapper)
+        {
+            _tagsData = tagsData;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        public async Task<ActionResult<List<Tags>>> Index()
+        {
+            var model = await _tagsData.GetAllAsync();
+            return model.ToList();
+        }
+    }
+}
